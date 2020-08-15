773855169a4d9ffc09cb6d864fb308abcb54cd97
Joshua Fehler <jsfehler@gmail.com>
2020-08-14 21:54:03 -0400

    Add character portraits

--- docs/examples/js/player_select.js
+++ docs/examples/js/player_select.js
@@ -28,6 +28,12 @@ function preload() {
     this.load.image("portraitBoxLeft", assetRoot + "portrait_box_left.png");
     this.load.image("portraitBoxRight", assetRoot + "portrait_box_right.png");
 
+    this.load.image("ppPortrait", assetRoot + "pp_portrait.png");
+    this.load.image("joPortrait", assetRoot + "jo_portrait.png");
+    this.load.image("msPortrait", assetRoot + "ms_portrait.png");
+    this.load.image("ckPortrait", assetRoot + "ck_portrait.png");
+    this.load.image("rndPortrait", assetRoot + "rnd_portrait.png");
+
     this.load.spritesheet(
         'selectHover', assetRoot + 'select_hover.png', { frameWidth: 40, frameHeight: 32 },
     );
@@ -58,31 +64,37 @@ function create() {
     randoButton.characterName = 'Random';
     randoButton.upEvent = ()=>{};
     randoButton.downEvent = ()=>{};
+    randoButton.portraitName = "rndPortrait";
 
     var pakpaoButton = new uiWidgets.Button(this, 0, 0, "pakpaoIcon");
     pakpaoButton.characterName = 'Pakpao';
     pakpaoButton.upEvent = ()=>{};
     pakpaoButton.downEvent = ()=>{};
+    pakpaoButton.portraitName = "ppPortrait";
 
     var kidButton = new uiWidgets.Button(this, 0, 0, "kidIcon");
     kidButton.characterName = 'Canuckle Kid';
     kidButton.upEvent = ()=>{};
     kidButton.downEvent = ()=>{};
+    kidButton.portraitName = "ckPortrait";
 
     var joanButton = new uiWidgets.Button(this, 0, 0, "joanIcon");
     joanButton.characterName = 'Joan the Drifter';
     joanButton.upEvent = ()=>{};
     joanButton.downEvent = ()=>{};
+    joanButton.portraitName = "joPortrait";
 
     var messengerButton = new uiWidgets.Button(this, 0, 0, "messengerIcon");
     messengerButton.characterName = 'The Messenger';
     messengerButton.upEvent = ()=>{};
     messengerButton.downEvent = ()=>{};
+    messengerButton.portraitName = "msPortrait";
 
     var randoButton2 = new uiWidgets.Button(this, 0, 0, "randoIcon");
     randoButton2.characterName = 'Random';
     randoButton2.upEvent = ()=>{};
     randoButton2.downEvent = ()=>{};
+    randoButton2.portraitName = "rndPortrait";
 
     this.playerButtons = [
       randoButton, pakpaoButton, kidButton, joanButton, messengerButton, randoButton2,
@@ -116,6 +128,11 @@ function create() {
         characterText.text = group.selected.characterName;
         selectHover.x = f.x;
         selectHover.y = f.y;
+
+        portraitSprite.setTexture(group.selected.portraitName);
+        portraitSpriteBack.setTexture(group.selected.portraitName);
+        tween.restart();
+        tween2.restart();
     };
 
     var nextItemCallback = function (group, context) {
@@ -123,10 +140,16 @@ function create() {
         characterText.text = group.selected.characterName;
         selectHover.x = f.x;
         selectHover.y = f.y;
+
+        portraitSprite.setTexture(group.selected.portraitName);
+        portraitSpriteBack.setTexture(group.selected.portraitName);
+        tween.restart();
+        tween2.restart();
     };
 
     // Attach left/right keyboardGroup
     var keyboardGroup = new uiWidgets.KeyboardGroup(this, true, this);
+    keyboardGroup.idx = 1;
     keyboardGroup.addNode(randoButton);
     keyboardGroup.addNode(pakpaoButton);
     keyboardGroup.addNode(kidButton);
@@ -140,6 +163,33 @@ function create() {
     // Add portrait boxes
     this.add.image(0, 240, "portraitBoxLeft").setOrigin(0.0, 1.0);
     this.add.image(320, 240, "portraitBoxRight").setOrigin(1.0, 1.0);
+
+    // Add portrait
+    var portraitSpriteBack = this.add.image(-204, 220, "ppPortrait").setOrigin(0.0, 1.0);
+    portraitSpriteBack.tint = 0x000000;
+
+    var portraitSprite = this.add.image(-208, 220, "ppPortrait").setOrigin(0.0, 1.0);
+
+    // Tween portrait sprite into position
+    var tween = this.tweens.add(
+        {
+            targets: portraitSprite,
+            ease: 'Cubic',
+            duration: 500,
+            repeat: 0,
+            x: 5,
+        }
+    )
+
+    var tween2 = this.tweens.add(
+        {
+            targets: portraitSpriteBack,
+            ease: 'Cubic',
+            duration: 800,
+            repeat: 0,
+            x: 0,
+        }
+    )
 
     // Add name boxes
     this.add.image(0, 240, "nameBoxLeft").setOrigin(0.0, 1.0);
